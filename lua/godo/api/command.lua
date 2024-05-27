afe2808426bdaa177485300e1ebc42abb4299973
Arthur Adolfo <arthur_adolfo@tutanota.com>
2024-05-27 10:58:31 -0300

    chore: fix arguments for GodoCreateItem

--- lua/godo/api/command.lua
+++ lua/godo/api/command.lua
@@ -2,7 +2,9 @@ local function GodoCreateItem(id, description)
     vim.fn.jobstart( "godo " .. id .. '"' .. description .. '"' )
 end
 
-vim.api.nvim_create_user_command("GodoCreateItem", GodoCreateItem, {
+vim.api.nvim_create_user_command("GodoCreateItem", function(opts)
+    GodoCreateItem(opts.fargs)
+end, {
     desc = "Create an item on godo",
     nargs = 2,
 })
